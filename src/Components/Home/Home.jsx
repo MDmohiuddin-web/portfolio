a59560744a164964d3c6322924c174527d47048e
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-07-25 03:05:36 +0600

    tool not added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,5 @@
-// import { useState } from "react";
+
+
 import About from "../About/About";
 import Contact from "../Contact/Contact";
 import Hero from "../Hero/Hero";
@@ -9,15 +10,7 @@ import Skills from "../skills/Skills";
 const Home = () => {
 
   
-//   const [Loading,setLoading]=useState(true);
-
-//   if (Loading) {
-//     return (
-//       <div className="h-screen flex justify-center items-center ">
-//        <span className="animate-pulse text-xl md:text-6xl text-[#0be890]">Loading ... </span>
-//       </div>
-//     );
-// }
+ 
 
   return (
     <div >
--- src/Components/ROOT/Root.jsx
+++ src/Components/ROOT/Root.jsx
@@ -3,16 +3,34 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Root = () => {
+  const [Loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const timeout=setTimeout(() => {
+      setLoading(false)
+    }, 2000);
     Aos.init();
+    return ()=>clearTimeout(timeout)
   }, []);
 
+
+
+
+  if (Loading) {
+    return (
+      <div className="h-screen flex justify-center items-center ">
+        <span className="animate-pulse text-xl md:text-6xl text-[#0be890] transition delay-300">
+          Loading ...
+        </span>
+      </div>
+    );
+  }
+
   return (
-    <div className="capitalize" data-aos="fade-down">
+    <div className="capitalize" >
       <div className="w-[90%]  m-auto capitalize font-roboto">
         <Navbar></Navbar>
         <Outlet></Outlet>
