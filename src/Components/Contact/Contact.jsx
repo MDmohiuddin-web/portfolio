8b03068d5c15a8f78ed6e283d163c3bbe10da664
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-09-24 22:04:15 +0600

    add some

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,7 +7,8 @@ const Contact = () => {
            Contact With Me
             </h2>
             <h3>{`Let's work together to build outstanding web experiences!`}</h3>
-            <p>As a dedicated Web Developer, I collaborate closely with my clients on every project. My goal is not only to deliver exceptional work that drives business growth but also to establish long-term relationships built on trust and genuine care. Let’s create something remarkable together!</p></div>
+            <p>As a dedicated Web Developer, I collaborate closely with my clients on every project. My goal is not only to deliver exceptional work that drives business growth but also to establish long-term relationships built on trust and genuine care. Let’s create something remarkable together!</p>
+            j</div>
             {/*  */}
            <div data-aos="fade-up" className=" md:w-[49%] flex flex-col text-left ">
             <h2 className="text-4xl md:text-6xl my-3 text-[#0be890]">
