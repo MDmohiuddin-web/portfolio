51760ccc1904318932b55325907e7977fa3ea5de
MOHMMED.MOHIUDDIN <149958768+MDmohiuddin-web@users.noreply.github.com>
2025-03-16 12:32:06 +0600

    sssss

--- src/utils/Whatsapp.jsx
+++ src/utils/Whatsapp.jsx
@@ -10,15 +10,9 @@ const Whatsapp = () => {
                 statusMessage="online" // Optional
                 allowClickAway={true} // Optional: To allow the widget to be closed when clicking outside
                 allowEsc={true} // Optional: To allow the widget to be closed when pressing "Esc"
-<<<<<<< HEAD
-
-                avatar="./src/Components/assets/profile-pic for web.png" // Optional: Path to your avatar image
-                chatMessage="Hi there! I'm available to chat about my web development services. Feel free to ask me anything."
-=======
                 
-                avatar="../../src/Components/assets/profile-pic for web.png" // Optional: Path to your avatar image
+                avatar="./src/Components/assets/profile-pic for web.png" // Optional: Path to your avatar image
              chatMessage="Hi there! I'm available to chat about my web development services. Feel free to ask me anything."
->>>>>>> f5ea44e8efb57fd942caa85025dbfc940dce4864
                 darkMode={false} // Optional: Enable dark mode
             />
 
